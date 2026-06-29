decab081f3b31ce905639a92ca1a998011c7fb07
Lydia McShane <lydiamcshane14@gmail.com>
2026-06-29 13:02:21 -0400

    made headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 
   use: {
     baseURL: 'https://www.saucedemo.com',
-    headless: false,
+    headless: true,
 
     screenshot: 'only-on-failure',
     trace: 'retain-on-failure',
